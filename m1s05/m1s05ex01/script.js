3d0c88c1a8c882bebc913a470e94a9ef3cc2ae7c
Alex Athayde Viana Ribeiro <alex_a_ribeiro@estudante.sesisenai.org.br>
2024-02-22 20:28:55 -0300

    Adicionando arquivo .gitignore para node_modules

---
+++ m1s05/m1s05ex01/script.js
@@ -0,0 +1,9 @@
+// prompt importa a biblioteca prompt-sync
+// instalei através do comando "npm install prompt-sync"
+const prompt = require('prompt-sync')();
+
+// array de frutas
+const frutas = ["Abacate", "Banana", "Maça"];
+
+// exibe a fruta do array na posição 2
+console.log(frutas[1]);
