7a1a1c7354ad8ed335fe5e4f4d85553c49557c37
Alex Athayde Viana Ribeiro <alex_a_ribeiro@estudante.sesisenai.org.br>
2024-03-02 20:22:25 -0300

    add M1s06ex4

--- m1s06/m1s06ex4/script.js
+++ m1s06/m1s06ex4/script.js
@@ -1,2 +1,17 @@
-const prompt = require("prompt-sync")({sigint: true});
-
+function salvarUsuario() {
+    const nome = prompt("Digite seu nome:");
+    const idade = parseInt(prompt("Digite sua idade:"));
+    const email = prompt("Digite seu email:");
+  
+    const usuario = {
+      nome,
+      idade,
+      email,
+    };
+  
+    localStorage.setItem("user", JSON.stringify(usuario));
+  
+    alert("Usuário salvo com sucesso!");
+  }
+  
+  salvarUsuario();
