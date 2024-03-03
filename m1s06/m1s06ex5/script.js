a5840f3e7d41abf8e5d5396d140da468ac484eb6
Alex Athayde Viana Ribeiro <alex_a_ribeiro@estudante.sesisenai.org.br>
2024-03-02 22:16:09 -0300

    add M1s06ex5

--- m1s06/m1s06ex5/script.js
+++ m1s06/m1s06ex5/script.js
@@ -1,2 +1,14 @@
-const prompt = require("prompt-sync")({sigint: true});
-
+fetch('https://api.thecatapi.com/v1/images/search?limit=10', {
+  method: "GET",
+})
+  .then((respostaFetch) => {
+    return respostaFetch.json();
+  })
+  .then((respostaApi) => {
+    for (let foto of respostaApi) {
+        document.write(`<img src='${foto.url}' width='200'/>`);
+    }
+  })
+  .catch((error) => {
+    console.error(error);
+  });
