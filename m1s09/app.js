6aa0884a4c4a16d36ecc3b5a2ae812e2d41000fb
Alex Athayde Viana Ribeiro <alex_a_ribeiro@estudante.sesisenai.org.br>
2024-04-01 21:31:25 -0300

    add exerc02

--- m1s09/app.js
+++ m1s09/app.js
@@ -10,6 +10,14 @@ app.get("/", (req, res) => {
   res.send("Servidor Online!!!");
 });
 
+app.get("/sobre", function (req, res) {
+  res.send("Essa é minha primeira aplicação de servidores!");
+});
+
+app.get("/contato", function (req, res) {
+  res.send("Essa é o nosso contato!");
+});
+
 app.listen(3000, () => {
   console.log("Servidor rodando na porta 3000");
 });
