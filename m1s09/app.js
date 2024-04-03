428bfc8256ccea38bd4bf04d027ebabc0cc74782
Alex Athayde Viana Ribeiro <alex_a_ribeiro@estudante.sesisenai.org.br>
2024-04-02 23:48:51 -0300

    revisado

--- m1s09/app.js
+++ m1s09/app.js
@@ -41,7 +41,7 @@ app.post("/produtos", logHoraMiddleware, (req, res) => {
   const produto = req.body;
   produto.id = produtos.length > 0 ? produtos[produtos.length - 1].id + 1 : 1;
   produtos.push(produto);
-  res.status(201).send("produto adicionada com sucesso.");
+  res.status(201).send("produto adicionado com sucesso.");
 });
 
 app.put("/produtos/:id", logHoraMiddleware, (req, res) => {
@@ -49,22 +49,22 @@ app.put("/produtos/:id", logHoraMiddleware, (req, res) => {
   const newData = req.body;
   const index = produtos.findIndex((produto) => produto.id === parseInt(id));
   if (index === -1) {
-    res.status(404).send("produto não encontrada.");
+    res.status(404).send("Produto não encontrado.");
     return;
   }
   produtos[index] = { ...produtos[index], ...newData };
-  res.status(200).send("produto atualizada com sucesso.");
+  res.status(200).send("Produto atualizado com sucesso.");
 });
 
 app.delete("/produtos/:id", logHoraMiddleware, (req, res) => {
   const { id } = req.params;
   const index = produtos.findIndex((produto) => produto.id === parseInt(id));
   if (index === -1) {
-    res.status(404).send("produto não encontrada.");
+    res.status(404).send("Produto não encontrado.");
     return;
   }
   produtos.splice(index, 1);
-  res.status(200).send("produto deletada com sucesso.");
+  res.status(200).send("Produto deletado com sucesso.");
 });
 
 app.listen(3000, function () {
