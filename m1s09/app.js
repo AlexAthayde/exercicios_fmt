70af62a3810650beb929344844e0fb23c64337f3
Alex Athayde Viana Ribeiro <alex_a_ribeiro@estudante.sesisenai.org.br>
2024-04-02 23:04:02 -0300

    add exerc06

--- m1s09/app.js
+++ m1s09/app.js
@@ -18,6 +18,8 @@ const logHoraMiddleware = (req, res, next) => {
 
 app.use(logHoraMiddleware);
 
+let produtos = [];
+
 app.get("/", logHoraMiddleware, function (req, res) {
   res.sendFile(__dirname + "/public/index.html");
 });
@@ -30,9 +32,39 @@ app.get("/contato", logHoraMiddleware, function (req, res) {
   res.send("Essa é o nosso contato!");
 });
 
-app.get("/produto/:id", function (req, res) {
+app.get("/produtos/:id", logHoraMiddleware, function (req, res) {
   const { id } = req.params;
   res.status(200).send(`Produto com id: ${id}`);
+});
+
+app.post("/produtos", logHoraMiddleware, (req, res) => {
+  const produto = req.body;
+  produto.id = produtos.length > 0 ? produtos[produtos.length - 1].id + 1 : 1;
+  produtos.push(produto);
+  res.status(201).send("produto adicionada com sucesso.");
+});
+
+app.put("/produtos/:id", logHoraMiddleware, (req, res) => {
+  const { id } = req.params;
+  const newData = req.body;
+  const index = produtos.findIndex((produto) => produto.id === parseInt(id));
+  if (index === -1) {
+    res.status(404).send("produto não encontrada.");
+    return;
+  }
+  produtos[index] = { ...produtos[index], ...newData };
+  res.status(200).send("produto atualizada com sucesso.");
+});
+
+app.delete("/produtos/:id", logHoraMiddleware, (req, res) => {
+  const { id } = req.params;
+  const index = produtos.findIndex((produto) => produto.id === parseInt(id));
+  if (index === -1) {
+    res.status(404).send("produto não encontrada.");
+    return;
+  }
+  produtos.splice(index, 1);
+  res.status(200).send("produto deletada com sucesso.");
 });
 
 app.listen(3000, function () {
